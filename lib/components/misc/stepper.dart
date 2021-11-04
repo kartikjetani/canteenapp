bf552da8351a33ffe6dfdf83d789bea6434d262e
Kartik Jetani <kartikjetani15@gmail.com>
2021-11-04 18:17:49 +0530

    order stepper added
    order menu, stepper,navigation

---
+++ lib/components/misc/stepper.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StepperDemo extends StatefulWidget {
+  @override
+  _StepperDemoState createState() => _StepperDemoState();
+}
+
+class _StepperDemoState extends State<StepperDemo> {
+  int _currentStep = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: Text('Order Status',
+              style: GoogleFonts.poppins(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.normal,
+                  color: Color.fromRGBO(62, 68, 98, 1))),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Stepper(
+                type: StepperType.vertical,
+                physics: ScrollPhysics(),
+                currentStep: _currentStep,
+                onStepTapped: (step) => tapped(step),
+                onStepContinue: continued,
+                onStepCancel: cancel,
+                steps: <Step>[
+                  Step(
+                    title: new Text('Account'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: 'Email Address'),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Password'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 0
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Address'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: 'Home Address'),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Postcode'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 1
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                  Step(
+                    title: new Text('Mobile Number'),
+                    content: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              InputDecoration(labelText: 'Mobile Number'),
+                        ),
+                      ],
+                    ),
+                    isActive: _currentStep >= 0,
+                    state: _currentStep >= 2
+                        ? StepState.complete
+                        : StepState.disabled,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  tapped(int step) {
+    setState(() => _currentStep = step);
+  }
+
+  continued() {
+    _currentStep < 2 ? setState(() => _currentStep += 1) : null;
+  }
+
+  cancel() {
+    _currentStep > 0 ? setState(() => _currentStep -= 1) : null;
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,69 +1,213 @@
 import 'package:canteenapp/components/menu_item_card.dart';
+import 'package:canteenapp/components/misc/stepper.dart';
 import 'package:canteenapp/components/primary_btn_red.dart';
 import 'package:canteenapp/models/fooditem_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Menu(),
+    OrderHistory(),
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     List<FoodItem> menu = [];
-    final Stream<QuerySnapshot> _menuStream =
-        FirebaseFirestore.instance.collection('food_items').snapshots();
 
     return Scaffold(
-        backgroundColor: Color.fromRGBO(248, 245, 242, 1),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70.0),
-          child: AppBar(
-            title: Center(
-                child: Text("Today's Menu",
-                    style: GoogleFonts.poppins(
-                        fontSize: 26,
-                        fontWeight: FontWeight.w500,
-                        fontStyle: FontStyle.normal,
-                        color: Color.fromRGBO(62, 68, 98, 1)))),
-            backgroundColor: Colors.white,
-            elevation: 0,
+      backgroundColor: Color.fromRGBO(248, 245, 242, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Menu',
+            backgroundColor: Colors.red,
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Order',
+            backgroundColor: Colors.green,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0),
+        child: AppBar(
+          title: Center(
+              child: Text("Canteen",
+                  style: GoogleFonts.poppins(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.normal,
+                      color: Color.fromRGBO(62, 68, 98, 1)))),
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-        body: Column(
-          children: [
-            Container(
-              height: size.height * 0.7,
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: _menuStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Something went wrong');
-                    }
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+    );
+  }
+}
 
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    }
+class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
 
-                    return ListView(
-                      children:
-                          snapshot.data!.docs.map((DocumentSnapshot document) {
-                        Map<String, dynamic> data =
-                            document.data()! as Map<String, dynamic>;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    final Stream<QuerySnapshot> _menuStream =
+        FirebaseFirestore.instance.collection('food_items').snapshots();
+    return Column(
+      children: [
+        Container(
+          height: size.height * 0.69,
+          child: StreamBuilder<QuerySnapshot>(
+              stream: _menuStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
 
-                        int cnt = 0;
-                        return (data["is_avail"])
-                            ? MenuItem(
-                                data: data,
-                              )
-                            : Container();
-                      }).toList(),
-                    );
-                  }),
-            ),
-            PrimaryBtn(child: Text("Checkout")),
-          ],
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+
+                    int cnt = 0;
+                    return (data["is_avail"])
+                        ? MenuItem(
+                            data: data,
+                          )
+                        : Container();
+                  }).toList(),
+                );
+              }),
+        ),
+        PrimaryBtn(
+          child: Text("Checkout"),
+          onPressed: () {
+            Get.to(StepperDemo());
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class OrderHistory extends StatelessWidget {
+  const OrderHistory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: [
+          HistoryCard(),
+          HistoryCard(),
+          HistoryCard(),
+          HistoryCard(),
+          HistoryCard(),
+          HistoryCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class HistoryCard extends StatelessWidget {
+  const HistoryCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+        child: Container(
+          child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 25),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Column(
+                              children: [Text("Date : 23/06/2002")],
+                            ),
+                          ]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "dhosa x 2",
+                                ),
+                                Text(
+                                  "open tost",
+                                ),
+                                Text("samosa")
+                              ],
+                            ),
+                            Column(
+                              children: [Text("25"), Text("56"), Text("88")],
+                            )
+                          ]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 5),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Column(
+                              children: [Text("Total : 250")],
+                            ),
+                          ]),
+                    ),
+                  ],
+                ),
+              )),
         ));
   }
 }
